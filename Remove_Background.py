07f81cb48924502bff3c6061cdf44b141f5c9e3f
ricarsur <rjosesuarez@unicesar.edu.co>
2023-09-22 17:14:35 -0500

    finished without interface

--- Remove_Background.py
+++ Remove_Background.py
@@ -1,10 +1,22 @@
+import tkinter as tk
+from tkinter import filedialog
+
 from rembg import remove
 
-input_path = 'unnamed.jpg'
-output_path= 'wdsa.png'
+ventana = tk.Tk()
+ventana.withdraw()
 
-with open(input_path, 'rb') as i:
+file = filedialog.askopenfilename(title = "Seleccione una imagen")
+
+if file:
+      input_path = file
+      output_path= 'resutado.png'
+
+      with open(input_path, 'rb') as i:
         with open(output_path, 'wb') as o:
             input = i.read()
             output = remove(input)
             o.write(output)
+      print(f"La imagen se ha procesado y guardado en {output_path}")
+else:
+    print("Ninguna imagen seleccionada")
